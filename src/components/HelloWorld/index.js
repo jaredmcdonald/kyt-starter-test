b7a9b1acad6729cd53a56262036b31c4dd74bc60
Price <carolyn.price@nytimes.com>
2016-09-01 13:53:01 -0400

    css and test updates

--- src/components/HelloWorld/index.js
+++ src/components/HelloWorld/index.js
@@ -3,10 +3,11 @@
 // React: https://facebook.github.io/react/
 
 import React from 'react';
+import styles from 'Hello.scss';
 
 function HelloWorld() {
   return (
-    <h1>Hello World!</h1>
+    <h1 className={styles.test} >Hello World!</h1>
   );
 }
 
